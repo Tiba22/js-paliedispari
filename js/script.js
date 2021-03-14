2121e413779b01bdf83731c931462a101af10daa
Alessandro Tibaldo <aletibo@gmail.com>
2021-03-14 16:09:22 +0100

    Esercizio 2 completato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // GOAL:
 // Una funzione per capire se la parola è
 // palindroma
+
+
 // L'utente sceglie pari o dispari e un numero
 // da 1 a 5.
 // Generiamo un numero random (sempre da 1
@@ -9,25 +11,67 @@
 // vinto.
 
 
-function funPalindroma(str) {
+//Esercizio Parola Palindroma
 
-  var reversed = str.split('').reverse().join('');
-  var pal = 'La tua parola è palindroma!';
-  var notPal = 'La tua parola non è palindroma!';
+// function funPalindroma(str) {
+//
+//   var reversed = str.split('').reverse().join('');
+//   var pal = 'La tua parola è palindroma!';
+//   var notPal = 'La tua parola non è palindroma!';
+//
+//   //Split per convertire in un array di lettere,
+//   // reverse per invertire,
+//   // join per riconvertire
+//
+//
+//   if (reversed === str) {
+//     return document.getElementById('maintitle').innerHTML = pal;
+//   }
+//
+//   else {
+//     return document.getElementById('maintitle').innerHTML = notPal;
+//   }
+// }
+//
+// funPalindroma(prompt("Inserisci un nome").toLowerCase());
 
-  //Split per convertire in un array di lettere,
-  // reverse per invertire,
-  // join per riconvertire
 
+//Esercizio 2
 
-  if (reversed === str) {
-    return document.getElementById('maintitle').innerHTML = pal;
+var numUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
+var parDisp = prompt('Scegli pari o dispari');
+
+console.log(numUtente, parDisp);
+
+function play(){
+  var numPC = Math.floor(Math.random() * 5) +1;
+  var sum = numUtente + numPC;
+  var risultato = 'Hai scelto il numero ' + numUtente + ', il PC ha scelto ' + numPC + '<br> Hai scelto ' + parDisp + ', la somma totale è ' + sum + '<br>';
+
+  console.log(numPC, numUtente, sum);
+
+  if (sum % 2 == 0 && parDisp == 'pari') {
+
+    document.getElementById('maintitle').innerHTML += risultato + 'Hai vinto!'
+
+  }else if (sum % 2 == 0 && parDisp == 'dispari') {
+
+    document.getElementById('maintitle').innerHTML += risultato + 'Il PC ha vinto!'
+
+  }else if (sum % 2 == 1 && parDisp == 'dispari') {
+
+    document.getElementById('maintitle').innerHTML += risultato + 'Hai vinto!'
+
+  }else {
+
+    document.getElementById('maintitle').innerHTML += risultato + 'Il PC ha vinto!'
+
   }
 
-  else {
-    return document.getElementById('maintitle').innerHTML = notPal;
-  }
+
+
+  // document.getElementById('maintitle').innerHTML = 'Hai scelto il numero ' + numUtente + ', il PC ha scelto ' + numPC + '<br>' + 'Hai scelto ' + parDisp + ', la somma totale è ' + sum + '<br>'
 
 }
 
-funPalindroma(prompt("Inserisci un nome").toLowerCase());
+play();
